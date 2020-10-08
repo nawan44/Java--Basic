2886c254f34e91c7160167339830a9fafb3bbd1a
nawan44 <rachmat.d.gunawan.com>
2020-10-09 06:59:05 +0700

    string args

---
+++ string_args.java
@@ -0,0 +1,9 @@
+package com.training;
+
+public class String {
+    public static void main(String[] args){
+        System.out.println(args[0])
+        //input value args on terminal
+        System.out.println("Hello World")
+    }
+}
