234d46ca609b199e1a295daef4adc07eb23b36c8
nawan44 <rachmat.d.gunawan.com>
2020-10-13 10:46:08 +0700

    variable

---
+++ variable.java
@@ -0,0 +1,17 @@
+package basic;
+
+public class MySelf{
+    //make variable
+    String name, address;
+    int age;
+    
+    //input variable
+    name = "Rachmat Gunawan";
+    address = "Bekasi";
+    age = 30;
+
+    System.out.println("Name: " + name);
+    System.out.println("Address: " + address);
+    System.out.println("Age: " + age);
+    
+}
