b97d114230b80ae7ebd750a5ea22c18b563f95fb
nawan44 <rachmat.d.gunawan.com>
2020-10-14 10:54:19 +0700

    arithmetic

---
+++ arithmetic.java
@@ -0,0 +1,37 @@
+package basic;
+
+public class arithmetic{
+    //make variable
+    public static void main(String[] args){
+        //arithmetic
+        int variable1 = 11;
+        int variable2 = 7;
+        int result;
+        //addition          
+        result = variable1 + variable2;
+        System.out.println(variable1 + " + " + variable2 + " = " + result);
+        //subtraction
+        result = variable1 - variable2;
+        System.out.println("%d - %d = %d \n" ,  variable1, variable2, result);
+        //multiplied 
+        result = variable1 * variable2;
+        System.out.println("%d x %d = %d \n" ,  variable1, variable2, result);
+     
+        //division 
+        result = variable1 / variable2;
+        System.out.println("%d / %d = %d \n" ,  variable1, variable2, result);
+
+        float a = 9;
+        float b = 2;
+        float result = a/b;
+        System.out.println(a + " / " + b + " = " + result);
+
+        //modulus 
+        result = variable1 % variable2;
+        System.out.println("%d %% %d = %d \n" ,  variable1, variable2, result);
+ 
+        //squared 
+        //squared root of
+    }
+    
+}
